0677c9a03ea13e1cac90e9c5f5fcdd3c0a0c95cb
JokerCompiler <gamingofworld@outlook.com>
2019-05-06 00:46:33 -0400

    Update exercise.h

--- Src/exercise.h
+++ Src/exercise.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <QPushButton>
 #include <QLabel>
+#include <QVector>
 #include <QLineEdit>
 #include <QTextEdit>
 
@@ -18,26 +19,98 @@ public:
         QString Answer_SbS;
     };
 
-    Exercise(std::vector<Question> qrc, QLineEdit * Exer_Input, QTextEdit * Exer_RichText,
-    QLabel * Exer_QuestionNum, QLabel * Exer_Recherche){
+    Exercise(QVector<Question> qrc, QLineEdit * Exer_Input, QTextEdit * Exer_RichText,
+    QLabel * Exer_QuestionNum, QLabel * Exer_Recherche, QPushButton * Exer_NextBtn){
         _exercice.questions = qrc;
         _exercice.Length = qrc.size();
         _exercice.Level = 0;
-        _exercice.BtnState = NEW;
         this->Exer_Input = Exer_Input;
         this->Exer_RichText = Exer_RichText;
         this->Exer_QuestionNum = Exer_QuestionNum;
         this->Exer_Recherche = Exer_Recherche;
+        this->Exer_NextBtn = Exer_NextBtn;
+    }
+
+    void LoadContent(int flag = NEXT){
+        switch (flag) {
+        case NEW:
+            {
+
+            }
+            break;
+        case REFRESH:
+            {
+                Exer_NextBtn->setText("Commencer la série d'exercises!");
+            }
+            break;
+        case CORRECT:
+            {
+
+            if(_exercice.Level == _exercice.questions.length()){
+                LoadContent(END);
+            }else
+                Exer_RichText->setText(Exer_RichText->toHtml() + "\nBonne réponse!\n ");
+                Exer_NextBtn->setText("Prochaine question");
+            }
+            break;
+        case FALSE:
+            {
+            if(_exercice.Level == _exercice.questions.length()){
+                LoadContent(END);
+            }else
+                Exer_RichText->setText(Exer_RichText->toHtml() + "\nMauvaise réponse!\n " + _exercice.questions[_exercice.Level].Answer_SbS);
+                Exer_NextBtn->setText("Prochaine question");
+            }
+            break;
+        case NEXT:
+            {
+            Question newQuestion = _exercice.questions[_exercice.Level];
+            Exer_RichText->setHtml(newQuestion.htmlText);
+            Exer_Recherche->setText(newQuestion.AnswerLabel);
+            Exer_QuestionNum->setText(_exercice.WhereAt);
+            Exer_NextBtn->setText("Valider");
+            }
+            break;
+        case END:
+            {
+
+            }
+            break;
+        }
+
+    }
+    void CheckAnswer(){
+        if(Exer_Input->text() != _exercice.questions[_exercice.Level].Answer){
+            LoadContent(FALSE);
+        }else{
+            LoadContent(CORRECT);
+        }
     }
 
     int getState(){
         return _exercice.BtnState;
     }
-
+    void FirstQuestion(){
+        LoadContent(REFRESH);
+    }
     void NextQuestion(){
-        _exercice.Level++;
-        _exercice.WhereAt = "Question " + QString::number(_exercice.Level+1) + "/" + QString::number(_exercice.Length);
-
+        if(Exer_NextBtn->text() == "Valider"){
+            CheckAnswer();
+            return;
+        }
+        if(_exercice.Level == _exercice.questions.length()){
+            LoadContent(END);
+            return;
+        }
+        if(Exer_NextBtn->text() == "Prochaine question"){
+            _exercice.Level++;
+            _exercice.WhereAt = "Question " + QString::number(_exercice.Level+1) + "/" + QString::number(_exercice.Length);
+            LoadContent();
+        }
+        if(Exer_NextBtn->text() == "Commencer la série d'exercises!"){
+            _exercice.WhereAt = "Question " + QString::number(_exercice.Level+1) + "/" + QString::number(_exercice.Length);
+            LoadContent();
+        }
     }
 
     static void Reset(Exercise* exer){
@@ -45,14 +118,15 @@ public:
     }
 
 private:
-    enum btnState {NEW,NEXT,END};
+    enum btnState {NEW, CORRECT, FALSE, NEXT,END, REFRESH};
     struct Exer{
-        std::vector<Question> questions;
+        QVector<Question> questions;
         QString WhereAt;
         int Level;
         int Length;
         btnState BtnState;
     };
+    QPushButton * Exer_NextBtn;
     QLineEdit * Exer_Input;
     QTextEdit * Exer_RichText;
     QLabel * Exer_QuestionNum;
